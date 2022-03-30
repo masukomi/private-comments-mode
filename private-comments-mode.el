3b134fa727ec78a6289f0195f1107495273e6ee3
masukomi <masukomi@masukomi.org>
2022-03-29 20:34:33 -0400

    added link to private comments server in commentary

--- private-comments-mode.el
+++ private-comments-mode.el
@@ -12,8 +12,10 @@
 
 ;;; Commentary:
 
-;; A minor mode for ``masukomi/private_comments``.  Private comments
-;; appear as overlays and are not part of the source.
+;; A minor mode for ``masukomi/private_comments``.
+;; https://github.com/masukomi/private_comments
+;; Private comments appear as overlays and are
+;; not part of the source.
 
 ;;; Code:
 
